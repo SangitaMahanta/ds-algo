a803d216bd9156930ef5c75965f9e82a956b1760
SangitaMahanta <sangitamiki1998@gmail.com>
2021-05-15 09:39:10 +0530

    selectionSort || insertionSort || mergeSort

--- src/main/java/org/insertionSort/InsertionSortMain.java
+++ src/main/java/org/insertionSort/InsertionSortMain.java
@@ -4,24 +4,46 @@ import java.util.Arrays;
 
 public class InsertionSortMain {
     public static void main(String[] args) {
+// it is also called as online sort
+//        best case O(n) for accending order
+//        worst case n(n-1)/2 = O(n^2)for decending order
 
                 int[] arr = {2, 33, 4, 11, 4, 66};
 //        int[] arr = {2, 4, 11, 33, 4, 66};
 
                 for (int i = 1; i < arr.length; i++) {
                     //    33
-                    int key = arr[i];
-                    int j = i - 1;
+                    int key = arr[i];   //33 4
+                    int j = i - 1;  // j=0 1
+                    //2  33
                     while (j >= 0 && key < arr[j]) {
-                        arr[j + 1] = arr[j];
-//                j = j - 1;
-                        j--;
+                        arr[j + 1] = arr[j]; // arr[2]=33
+                j = j - 1;
+                        j--;// 0
                     }
 
                     arr[j + 1] = key;
 
                 }
-                System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
+
+
+//        int arr1[]={23,12,40,30,15};
+//        int a=1;
+//        while(a<arr1.length){
+//            int b=a-1;
+//            int key=arr1[a];
+//            while (b>=0 && arr1[b]>key){
+//                arr1[b+1]=arr1[b];
+//                b--;
+//            }
+//            arr1[b+1]=key;
+//            a++;
+//        }
+//                System.out.println(Arrays.toString(arr1));
+
             }
+
+
 
 }
---
+++ src/main/java/org/mergeSort/MergeSortMain.java
@@ -0,0 +1,67 @@
+package org.mergeSort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MergeSortMain {
+
+
+    public static void mergingTwoArray(int arr[],int l_index,int mid_index,int r_index){
+        int arr1[]=new int[20];
+        int i=l_index;
+        int j=mid_index+1;
+        int k=l_index;
+
+        while (i<=mid_index && j<=r_index){
+            if(arr[i]<arr[j]){
+                arr1[k]=arr[i];
+                i++;
+            }
+            else {
+                 arr1[k]=arr[j];
+                 j++;
+            }
+            k++;
+        }
+
+        //if left array is sorted
+        if(i>mid_index){
+            while (j<=r_index){
+                arr1[k]=arr[j];
+                j++;
+                k++;
+            }
+
+        }else {
+            while (i<=mid_index){
+                arr1[k]=arr[i];
+                i++;
+                k++;
+            }
+        }
+for (k=l_index;k<=r_index;k++){
+    arr[k]=arr1[k];
+}
+
+    }
+
+
+    public static void mergeSort(int arr[],int l_index,int r_index){
+        if (l_index < r_index) {
+            int mid_index=(l_index+r_index)/2;
+            mergeSort(arr,l_index,mid_index);
+            mergeSort(arr,mid_index+1,r_index);
+            mergingTwoArray(arr,l_index,mid_index,r_index);
+        }
+
+
+    }
+    public static void main(String[] args) {
+        int arr[]={9,4,7,6,3,1,5};
+        int l_index=0;
+        int r_index=arr.length-1;
+        System.out.println("Before sorting array is : "+ Arrays.toString(arr));
+        mergeSort(arr,l_index,r_index);
+        System.out.println("After sorting array is : "+ Arrays.toString(arr));
+    }
+}
