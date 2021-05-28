1aae6832047bf24faeea9c6afd23d114d4b62838
SangitaMahanta <sangitamiki1998@gmail.com>
2021-05-28 23:31:04 +0530

    shortest path

---
+++ src/main/java/org/Test/DistanceCoverBySara.java
@@ -0,0 +1,36 @@
+package org.Test;
+
+import java.util.Scanner;
+
+public class DistanceCoverBySara {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        double res=0;
+        int i=2;
+        if (n % 2 == 0) {
+            if (n % 4 == 2)
+                res = 1;
+            else if (n % 4 == 0)
+                res = 0;
+        }
+        else
+            res = dis(n);
+
+        System.out.println("The shortest distance is : "+(int)res);
+    }
+
+
+
+
+    public static double dis(double n) {
+            if (n == 1)
+                return 1;
+            else
+                return n - dis(n - 1) * Math.pow(-1, (n - 1));
+
+        }
+}
+
+
+
