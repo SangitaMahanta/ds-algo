9c2bb47771389403a275b2727b629cf4d62dd5ab
SangitaMahanta <sangitamiki1998@gmail.com>
2021-07-11 21:05:13 +0530

    finding kth smallest and largest element

---
+++ src/main/java/org/queue/KthSmallestAndLargest.java
@@ -0,0 +1,33 @@
+package org.queue;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+//kth max and min element in an array
+public class KthSmallestAndLargest {
+    public static void main(String[] args) {
+        int arr[] = {7,21,13,6,25,71};
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>();
+        int k = 3;
+        for (int i=0; i<k+1; i++){
+            priorityQueue.add(arr[i]);
+        }
+        int index = 0;
+        for (int i = k+1; i< arr.length; i++){
+            arr[index++] = priorityQueue.peek();
+            priorityQueue.poll();
+            priorityQueue.add(arr[i]);
+        }
+
+        Iterator<Integer>itr = priorityQueue.iterator();
+        while (itr.hasNext()){
+            arr[index++] = priorityQueue.peek();
+            priorityQueue.poll();
+        }
+        System.out.println("kth sorted array ");
+        System.out.println(Arrays.toString(arr));
+//        System.out.println("kth smallest element "+arr[k-1]);
+//        System.out.println("kth largest element "+arr[arr.length-k-1]);
+
+    }
+}
